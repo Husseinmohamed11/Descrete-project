bf5b05d93a86daec48ad3a4c2fb6742d985f8d9a
Husseinmohamed11 <121402159+Husseinmohamed11@users.noreply.github.com>
2022-12-25 21:27:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+     static void Main(string[] args)
+    {
+        //Perfect Numbers//
+
+
+        Console.WriteLine("Enter n1 & n2");
+        int n1 = Convert.ToInt32(Console.ReadLine());
+        int n2 = Convert.ToInt32(Console.ReadLine());
+        int sum;
+
+        for (int i = n1; i <= n2; i++)
+        {
+            int j = 1;
+            sum = 0;
+            while (j < i)
+            {
+                if (i % j == 0)
+                {
+                    sum = sum + j;
+
+                }
+                j++;
+            }
+            if (sum == i)
+                Console.WriteLine("{0}", i);
+
+
+        }
+    }
+}
